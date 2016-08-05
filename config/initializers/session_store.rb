8e173d4efb9141158aceebbeed8a40efc3a7050c
sven liu <sven@svendeMacBook-Pro.local>
2016-08-05 15:04:32 +0800

    modify session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_chat_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_chat_session', domain: {
+  production: 'sven.remote',
+  development: 'sven.local'
+}.fetch(Rails.env.to_sym, :all)
