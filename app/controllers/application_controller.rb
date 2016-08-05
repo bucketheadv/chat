ba52718b7f55b73bb380748e6c60dfd2a4586731
sven liu <sven@svendeMacBook-Pro.local>
2016-08-05 15:45:12 +0800

    modify protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
-
-  after_action :set_csrf_cookie_for_ng
-
-  def intercept_html_requests
-    redirect_to('/') if request.format == Mime::HTML
-  end
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
-
-  rescue_from ActionController::InvalidAuthenticityToken do |exception|
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-    message = 'Rails CSRF token error, please try again'
-    render_with_protection(message.to_json, {:status => :unprocessable_entity})
-  end
-
-  def render_with_protection(object, parameters = {})
-    render parameters.merge(content_type: 'application/json', text: ")]}',\n" + object.to_json)
-  end
-
-  protected
-
-  def verified_request?
-    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
-  end
 end
